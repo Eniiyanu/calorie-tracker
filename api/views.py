0c7b7e362e0f32b9bd3e5c5d5fb6a8d1e6ff823b
eniiyanu <depopf89@gmail.com>
2024-10-22 22:34:49 +0100

    Added the urls and admin permission

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Meal
+from .serializers import MealSerializer
+from .permissions import IsOwnerOrAdmin
 
-# Create your views here.
+class MealViewSet(viewsets.ModelViewSet):
+    serializer_class = MealSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+    
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Meal.objects.all()
+        return Meal.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
